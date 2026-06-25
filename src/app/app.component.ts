9d19050ac83eb8cd6089cce4cc635f013c8dc8ef
danilavolkov <292004478+danilavolkov@users.noreply.github.com>
2026-06-26 01:02:24 +0300

    all4

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import {NgIf} from "@angular/common";
+import {NgFor} from "@angular/common";
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [FormsModule, NgIf, NgFor],
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+    name = "Джанет";
+    platform = "Patreon";
+    text = "Самая продоваемая сейчас";
+    count: number = 0;
+    increase($event : any): void {
+        this.count++;
+        console.log($event);
+    }
+public isAdmin: boolean = true;
+public num: number = 10;
+public day: number = 20;
+public lang: string = 'de';
+public age: number = 12;
+public arr: number[] = [1, 2, 3, 4, 5];
+public arr2: string[] = ['a', 'b', 'c', 'd'];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
